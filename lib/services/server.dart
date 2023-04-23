ab97f0a7e02f664ba3ee302f48ea6aa872e81e66
Eamce <eamsanroel19@gmail.com>
2023-04-23 23:54:01 +0800

    initial commit

--- lib/services/server.dart
+++ lib/services/server.dart
@@ -8,12 +8,12 @@ class Server {
   // static String categories_img = "http://192.168.100.41/onlinecakeshop/uploads/categories/";
 
 //office ip
-//     static String url = "http://172.16.43.125/cakecreations/";
-//     static String image_url = "http://172.16.43.125/onlinecakeshop/uploads/products/";
-//     static String categories_img = "http://172.16.43.125/onlinecakeshop/uploads/categories/";
+    static String url = "http://172.16.43.125/cakecreations/";
+    static String image_url = "http://172.16.43.125/onlinecakeshop/uploads/products/";
+    static String categories_img = "http://172.16.43.125/onlinecakeshop/uploads/categories/";
 
 // master ip
-static String url = "http://192.168.0.101/cakecreations/";
-static String image_url = "http://192.168.0.101/onlinecakeshop/uploads/products/";
-static String categories_img = "http://192.168.0.101/onlinecakeshop/uploads/categories/";
+// static String url = "http://192.168.0.101/cakecreations/";
+// static String image_url = "http://192.168.0.101/onlinecakeshop/uploads/products/";
+// static String categories_img = "http://192.168.0.101/onlinecakeshop/uploads/categories/";
 }
