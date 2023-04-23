c6c5571132ce5a8da81822894ff7189eb47c3641
Eamce <eamsanroel19@gmail.com>
2023-04-23 23:52:55 +0800

    initial commit

--- lib/Colors/Hex_Color.dart
+++ lib/Colors/Hex_Color.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll("#", "");
@@ -8,6 +7,5 @@ class HexColor extends Color {
     }
     return int.parse(hexColor, radix: 16);
   }
-
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
